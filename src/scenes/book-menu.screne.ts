50f4afd34029df31d0bc0d8980e2925a42b60e45
iadaria <jadarya@mail.ru>
2023-07-13 02:38:34 +0900

    init menu

--- src/scenes/book-menu.screne.ts
+++ src/scenes/book-menu.screne.ts
@@ -9,16 +9,27 @@ import {
   Sender,
 } from 'nestjs-telegraf';
 import { Context } from 'src/common/interfaces/context.interface';
-import { Update } from 'telegraf/typings/core/types/typegram';
+import {
+  InlineKeyboardButton,
+  Update,
+} from 'telegraf/typings/core/types/typegram';
 import { SceneContext } from 'telegraf/typings/scenes';
 import { MENU_BOOK } from 'src/common/common.constants';
 import { CHAPTER_01 } from 'src/data/chapter01';
+
+const inlineKeyboard: InlineKeyboardButton[][] = [
+  [{ text: 'Слова', callback_data: 'words' }],
+  [
+    { text: 'yandex п.', callback_data: 'yandexTranslate' },
+    { text: 'Авторский п.', callback_data: 'authorText' },
+  ],
+];
 
 @Scene(MENU_BOOK)
 export class BookMenuScene {
   @SceneEnter()
   async onSceneEnter(ctx: Context) {
-    ctx.reply(CHAPTER_01.text);
+    ctx.reply('The Silo');
   }
 
   @SceneLeave()
@@ -29,5 +40,25 @@ export class BookMenuScene {
   @Command('leave')
   async onLeaveCommand(ctx: Context): Promise<void> {
     await ctx.scene.leave();
+  }
+
+  @Command(['text'])
+  async onText(ctx: Context): Promise<void> {
+    await ctx.reply(CHAPTER_01.engText, {
+      reply_markup: { inline_keyboard: inlineKeyboard },
+    });
+  }
+
+  @Action(/words|yandex|author/)
+  async onAnswer(
+    @Ctx() context: SceneContext & { update: Update.CallbackQueryUpdate },
+  ) {
+    const cbQuery = context.update.callback_query;
+    const userAnswer = 'data' in cbQuery ? cbQuery.data : null;
+    const text = userAnswer ? CHAPTER_01[userAnswer] : '';
+    
+    await context.editMessageText(CHAPTER_01.engText + text, {
+      reply_markup: { inline_keyboard: inlineKeyboard },
+    });
   }
 }
---
+++ todo.md
@@ -0,0 +1,3 @@
+# 0
+1 Make the choosing of words for showing transcription. (But it should be settings ?)
+2 Make menu to show words or translate
